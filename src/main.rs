cbea16db4af1ab9440fb02796ab511cdfb52497a
kkstas <109525588+kkstas@users.noreply.github.com>
2023-10-05 23:10:12 +0200

    axum cookie test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::{middleware, response::Response, routing::get_service, Router};
 use std::net::SocketAddr;
+use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
 pub use self::error::{Error, Result};
@@ -12,6 +13,7 @@ async fn main() {
     let routes_all = Router::new()
         .merge(web::routes_login::routes())
         .layer(middleware::map_response(main_response_mapper))
+        .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     axum::Server::bind(&addr)
