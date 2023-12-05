be00ddd2c15546bd44879974821b15fdd6117a65
kkstas <stasiakkamilkrzysztof@gmail.com>
2023-12-05 21:09:57 +0100

    Upgrade Axum 0.6 -> 0.7

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use axum::{middleware, response::Response, routing::get_service, Router};
+use crate::ctx::Ctx;
+use crate::log::log_request;
+use axum::http::{Method, Uri};
+use axum::{
+    middleware,
+    response::{IntoResponse, Response},
+    routing::get_service,
+    Json, Router,
+};
 use model::ModelController;
-use std::net::SocketAddr;
+use serde_json::json;
+use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
-pub use self::error::{Error, Result};
+pub use self::error::{ClientError, Error, Result};
 
 mod ctx;
 mod error;
+mod log;
 mod model;
 mod web;
 
@@ -15,13 +26,11 @@ mod web;
 async fn main() -> Result<()> {
     let mc = ModelController::new().await?;
 
-    // Router utworzony osobno, aby tylko tu działało auth
     let routes_apis = web::routes_tickets::routes(mc.clone())
         .route_layer(middleware::from_fn(web::mw_auth::mw_require_auth));
 
     let routes_all = Router::new()
         .nest("/api", web::routes_login::routes())
-        // .nest("/api", web::routes_tickets::routes(mc.clone()))
         .nest("/api", routes_apis)
         .layer(middleware::map_response(main_response_mapper))
         .layer(middleware::from_fn_with_state(
@@ -30,18 +39,48 @@ async fn main() -> Result<()> {
         ))
         .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    axum::Server::bind(&addr)
-        .serve(routes_all.into_make_service())
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    axum::serve(listener, routes_all.into_make_service())
         .await
         .unwrap();
     Ok(())
 }
 
-async fn main_response_mapper(res: Response) -> Response {
+async fn main_response_mapper(
+    req_method: Method,
+    uri: Uri,
+    ctx: Option<Ctx>,
+    res: Response,
+) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
-    println!("");
-    res
+    let uuid = Uuid::new_v4();
+
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error":{
+                "type": client_error.as_ref(),
+                "req_uuid": uuid.to_string(),
+            }
+            });
+            println!("  ->> client_error_body: {client_error_body}");
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    // -- TODO: Build and log the server log line
+    let client_error = client_status_error.unzip().1;
+    log_request(uuid, req_method, uri, ctx, service_error, client_error)
+        .await
+        .unwrap();
+    println!("  ->> server log line - {uuid} - Error: {service_error:?}");
+
+    println!();
+    error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use axum::body::Body;
 use axum::http::request::Parts;
 use axum::{extract::FromRequestParts, http::Request, middleware::Next, response::Response};
 use lazy_regex::regex_captures;
@@ -8,11 +9,7 @@ use crate::ctx::Ctx;
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
-pub async fn mw_require_auth<B>(
-    ctx: Result<Ctx>,
-    req: Request<B>,
-    next: Next<B>,
-) -> Result<Response> {
+pub async fn mw_require_auth(ctx: Result<Ctx>, req: Request<Body>, next: Next) -> Result<Response> {
     println!("->> {:<12} - mw_require_auth", "MIDDLEWARE");
 
     ctx?;
@@ -20,10 +17,10 @@ pub async fn mw_require_auth<B>(
     Ok(next.run(req).await)
 }
 
-pub async fn mw_ctx_resolver<B>(
+pub async fn mw_ctx_resolver(
     cookies: Cookies,
-    mut req: Request<B>,
-    next: Next<B>,
+    mut req: Request<Body>,
+    next: Next,
 ) -> Result<Response> {
     println!("->> {:<12} - mw_ctx_resolver", "MIDDLEWARE");
 
@@ -42,7 +39,7 @@ pub async fn mw_ctx_resolver<B>(
 
     // remove the cookie if something went wrong other than NoAuthTokenCookie
     if result_ctx.is_err() && !matches!(result_ctx, Err(Error::AuthFailNoAuthTokenCookie)) {
-        cookies.remove(Cookie::named(AUTH_TOKEN))
+        cookies.remove(Cookie::from(AUTH_TOKEN))
     }
     // Store the ctx_result in the request extension
     req.extensions_mut().insert(result_ctx);
